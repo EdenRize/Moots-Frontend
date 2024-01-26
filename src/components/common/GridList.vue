16e677769a8b8611b99772765dc63209600eb456
Nadav Vazana <nadavvivsa@gmail.com>
2024-01-26 20:53:57 +0200

    grid list changes

--- src/components/common/GridList.vue
+++ src/components/common/GridList.vue
@@ -1,15 +1,18 @@
 <template>
-  <section
-    class="grid-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
-  >
+  <section :class="gridListClass">
     <slot name="items"></slot>
   </section>
 </template>
 
 <script lang="ts" setup>
+import { computed } from "vue";
 interface GridListProps {}
 
-const props = defineProps<GridListProps>();
+defineProps<GridListProps>();
+
+const gridListClass = computed(() => {
+  return "grid-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4";
+});
 </script>
 
 <style lang="scss" scoped></style>
