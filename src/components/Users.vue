80d805b9bdb7e5ac779bfedb3bac6a4755588d88
EdenRize <edenrize@gmail.com>
2024-01-26 17:38:20 +0200

    Init

---
+++ src/components/Users.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="users">
+            <input type="text" >
+    </section>
+</template>
+
+<script lang="ts" setup >
+
+
+interface UserProps {
+    
+}
+
+
+
+defineProps<UserProps>()
+
+</script>
+
+<style lang="scss" scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import routes from '../routes'; // Import your router instance
+import './scss/style.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(routes); // Use the router plugin
+
+app.mount('#app');
